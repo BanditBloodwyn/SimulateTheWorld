e19ec8d9c7a565606dd81e8abd7ab8325219b8e8
BanditBloodwyn <kevin@eichenbergs.info>
2022-03-20 14:52:25 +0100

    Projektdateien hinzufügen.

---
+++ SimulateTheWorld.GUI/Controls/Core/CoreControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimulateTheWorld.GUI.Controls.Core
+{
+    /// <summary>
+    /// Interaktionslogik für MainControl.xaml
+    /// </summary>
+    public partial class CoreControl : UserControl
+    {
+        public CoreControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
