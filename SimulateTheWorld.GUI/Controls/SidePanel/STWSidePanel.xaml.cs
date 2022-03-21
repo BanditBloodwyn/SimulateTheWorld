82b9d4a6b4621566ff4872283e9f32d024aebaa6
BanditBloodwyn <kevin@eichenbergs.info>
2022-03-21 15:51:28 +0100

    RenderView, Klassen umsortiert

---
+++ SimulateTheWorld.GUI/Controls/SidePanel/STWSidePanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimulateTheWorld.GUI.Controls.SidePanel
+{
+    /// <summary>
+    /// Interaktionslogik für STWSidePanel.xaml
+    /// </summary>
+    public partial class STWSidePanel : UserControl
+    {
+        public STWSidePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
