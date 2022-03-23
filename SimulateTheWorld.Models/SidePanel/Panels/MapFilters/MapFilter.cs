c30196814475e595276063de8068619aecb9bf69
BanditBloodwyn <kevin@eichenbergs.info>
2022-03-23 12:23:56 +0100

    MapFiltersControl und -Model Margin für Stackpanel im STWSidePanel

---
+++ SimulateTheWorld.Models/SidePanel/Panels/MapFilters/MapFilter.cs
@@ -0,0 +1,29 @@
+﻿using SimulateTheWorld.Models.Core;
+
+namespace SimulateTheWorld.Models.SidePanel.Panels.MapFilters;
+
+public class MapFilter : BaseViewModel
+{
+    private bool _active;
+
+    public string Name { get; }
+
+    public bool Active
+    {
+        get => _active;
+        set
+        {
+            if (_active != value)
+            {
+                _active = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public MapFilter(string name, bool active = false)
+    {
+        Name = name;
+        Active = active;
+    }
+}
---
+++ SimulateTheWorld.Models/SidePanel/Panels/MapFilters/MapFiltersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using SimulateTheWorld.Models.Core;
+
+namespace SimulateTheWorld.Models.SidePanel.Panels.MapFilters;
+
+public class MapFiltersViewModel : BaseViewModel
+{
+    public ObservableCollection<MapFilter> MapFilters { get; }
+
+    public MapFiltersViewModel()
+    {
+        MapFilters = new ObservableCollection<MapFilter>();
+        InitializeMapFilters();
+    }
+
+    private void InitializeMapFilters()
+    {
+        MapFilters.Add(new MapFilter("Population by tribe", true));
+        MapFilters.Add(new MapFilter("Countries"));
+        MapFilters.Add(new MapFilter("Life standard"));
+        MapFilters.Add(new MapFilter("Urbanization"));
+    }
+}
