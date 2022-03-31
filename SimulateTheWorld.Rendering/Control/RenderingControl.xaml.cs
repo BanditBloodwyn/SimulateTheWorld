825926b414f08b56a1cb3416139b356d4fdc4569
BanditBloodwyn <kevin@eichenbergs.info>
2022-03-31 15:25:52 +0200

    Erste OpenGL-Tests, Shader, RenderingControlViewModel unnötig und entfernt

--- SimulateTheWorld.Rendering/Control/RenderingControl.xaml.cs
+++ SimulateTheWorld.Rendering/Control/RenderingControl.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows.Controls;
-using SimulateTheWorld.Rendering.Model;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using OpenTK.Windowing.Common;
+using OpenTK.Wpf;
+using SimulateTheWorld.Rendering.Rendering;
 
 namespace SimulateTheWorld.Rendering.Control
 {
@@ -8,10 +12,35 @@ namespace SimulateTheWorld.Rendering.Control
     /// </summary>
     public sealed partial class RenderingControl : UserControl
     {
+        private readonly OpenGLRenderer _renderer;
+
         public RenderingControl()
         {
             InitializeComponent();
-            AddChild((DataContext as RenderingControlViewModel)?.GLControl);
+
+            var mainSettings = new GLWpfControlSettings { MajorVersion = 4, MinorVersion = 5, GraphicsProfile = ContextProfile.Compatability, GraphicsContextFlags = ContextFlags.Debug};
+            GlControl.Start(mainSettings);
+            _renderer = new OpenGLRenderer();
+        }
+
+        private void GlControl_OnRender(TimeSpan elapsedTimeSpan)
+        {
+            _renderer.OnRender(elapsedTimeSpan);
+        }
+
+        private void GlControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _renderer.OnLoaded();
+        }
+
+        private void GlControl_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _renderer.OnUnLoaded();
+        }
+
+        private void GlControl_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _renderer.UpdateViewPort(GlControl.ActualWidth, GlControl.ActualHeight);
         }
     }
 }
--- SimulateTheWorld.Rendering/Rendering/OpenGLRenderer.cs
+++ SimulateTheWorld.Rendering/Rendering/OpenGLRenderer.cs
@@ -1,17 +1,100 @@
 ﻿using System;
 using OpenTK.Graphics.ES30;
 using OpenTK.Mathematics;
-using SimulateTheWorld.World;
+using SimulateTheWorld.Rendering.Shaders;
 
 namespace SimulateTheWorld.Rendering.Rendering;
 
 public class OpenGLRenderer
 {
-    public void Render(TimeSpan elapsedTime)
+    private Shader shader;
+
+    #region Tests
+
+    private float[] _vertices =
     {
+        0.5f, 0.5f, 0.0f, // top right
+        0.5f, -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f // top left
+    };
+
+    private uint[] _indices =
+    {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3 // second triangle
+    };
+
+    private int _vertexBufferObject;
+    private int _vertexArrayObject;
+    private int _elementBufferObject;
+
+    #endregion
+
+    public void OnLoaded()
+    {
+        // create vertex and fragment shaders
+        shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+        shader.Use();
+
+        // generate VBO to send vertex data to the GPU
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+        // generate VAO
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        // set vertex attributes (how to interpret the vertex data)
+        GL.VertexAttribPointer(shader.GetAttributeLocation("aPosition"), 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        // generate an EBO to reuse _vertices
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
         GL.ClearColor(new Color4(0, 0, 70, 0));
+    }
+
+    public void OnRender(TimeSpan elapsedTime)
+    {
         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-        TerrainTile[,] terrainTiles = STWWorld.Instance.Terrain.Tiles;
+        //TerrainTile[,] terrainTiles = STWWorld.Instance.Terrain.Tiles;
+
+        if (shader == null)
+            return;
+
+        shader.Use();
+
+        TestRendering(elapsedTime);
+        
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, (IntPtr)0);
+        GL.Finish();
+    }
+
+    private void TestRendering(TimeSpan elapsedTime)
+    {
+        for (int i = 0; i < _vertices.Length; i++)
+        {
+            _vertices[i] += elapsedTime.Milliseconds / 10000f;
+        }
+    }
+
+    public void OnUnLoaded()
+    {
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.DeleteBuffer(_vertexBufferObject);
+
+        shader.Dispose();
+    }
+
+    public void UpdateViewPort(double width, double height)
+    {
+        GL.Viewport(0, 0, (int)width, (int)height);
     }
 }
