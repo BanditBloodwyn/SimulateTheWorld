754a0078e087d87ee2f3d1caed15e21c3478c8d0
BanditBloodwyn <kevin@eichenbergs.info>
2022-05-18 15:22:18 +0200

    Tabs für MarkedTileControl

---
+++ SimulateTheWorld.GUI.Controls/Controls/DisplayControls/CustomTabControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimulateTheWorld.GUI.Controls.Controls.DisplayControls
+{
+    /// <summary>
+    /// Interaktionslogik für CustomTabControl.xaml
+    /// </summary>
+    public partial class CustomTabControl : TabControl
+    {
+        public CustomTabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimulateTheWorld.GUI.Controls/Controls/DisplayControls/ValueDisplayControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimulateTheWorld.GUI.Controls.Controls.DisplayControls
+{
+    /// <summary>
+    /// Interaktionslogik für ValueDisplayControl.xaml
+    /// </summary>
+    public partial class ValueDisplayControl : Grid
+    {
+        public ValueDisplayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
