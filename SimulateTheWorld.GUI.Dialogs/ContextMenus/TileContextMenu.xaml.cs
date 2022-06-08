41108d52b12be8cd7add2cb39ca967c02b2f102b
BanditBloodwyn <kevin@eichenbergs.info>
2022-06-08 09:41:51 +0200

    OpenTileContextMenu

---
+++ SimulateTheWorld.GUI.Dialogs/ContextMenus/TileContextMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimulateTheWorld.GUI.Dialogs.ContextMenus
+{
+    /// <summary>
+    /// Interaktionslogik für TileContextMenu.xaml
+    /// </summary>
+    public partial class TileContextMenu : ContextMenu
+    {
+        public TileContextMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimulateTheWorld.GUI.ViewModels/ViewModels/RenderingControl/RenderingControlViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SimulateTheWorld.Core.GUI.MVVM;
+using SimulateTheWorld.GUI.ViewModels.Commands.RenderingControl;
+
+namespace SimulateTheWorld.GUI.ViewModels.ViewModels.RenderingControl;
+
+public class RenderingControlViewModel : ObservableObject
+{
+    public OpenTileContextMenuCommand OpenTileContextMenuCommand { get; }
+    
+    public RenderingControlViewModel()
+    {
+        OpenTileContextMenuCommand = new OpenTileContextMenuCommand();
+    }
+}
