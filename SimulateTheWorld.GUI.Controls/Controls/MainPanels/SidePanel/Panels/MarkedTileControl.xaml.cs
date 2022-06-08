71ae1d0a10318205b8cb6119a9b5261c2d4e964c
BanditBloodwyn <kevin@eichenbergs.info>
2022-06-08 14:14:19 +0200

    Anzeige angepinnte Location gefixt

--- SimulateTheWorld.GUI.Controls/Controls/MainPanels/SidePanel/Panels/MarkedTileControl.xaml.cs
+++ SimulateTheWorld.GUI.Controls/Controls/MainPanels/SidePanel/Panels/MarkedTileControl.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows.Controls;
-
-namespace SimulateTheWorld.GUI.Controls.Controls.MainPanels.SidePanel.Panels;
+﻿namespace SimulateTheWorld.GUI.Controls.Controls.MainPanels.SidePanel.Panels;
 
 /// <summary>
 /// Interaktionslogik für MarkedTileControl.xaml
 /// </summary>
-public partial class MarkedTileControl : UserControl
+public partial class MarkedTileControl
 {
     public MarkedTileControl()
     {
